12583333bf6ba4ced8680d09474f79f1d4f80a3a
tyee <2528447065@qq.com>
2024-01-06 18:32:12 +0800

    test

--- src/core/event.ts
+++ src/core/event.ts
@@ -1,1 +1,20 @@
 //TODO: 完成事件流的抽象
+
+import EventEmitter from 'events';
+
+// 创建 EventEmitter 实例
+const myEmitter = new EventEmitter();
+
+// 注册事件监听器
+myEmitter.on('event', () => {
+    console.log('An event occurred!');
+});
+
+// 触发事件
+myEmitter.emit('event');
+
+
+export class IssacEventer {
+
+}
+
